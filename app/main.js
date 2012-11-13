6c9d23774186bca2b1525306de6635ec838a3943
Matthew Danskey <mdanskey@gmail.com>
2012-11-13 10:12:48 -0800

    events now work

--- app/main.js
+++ app/main.js
@@ -1,25 +1,36 @@
 //routes
 var BeerRouter = Backbone.Router.extend({
   routes: {
-            "": "index"
-          },
+    "": "index"
+  },
 
   index: function() {
-          //alert("index!");
-          console.log('index route achieved');
-         }
+    var bv = new BeerView();
+    $('#main').append(bv.$el);
+    bv.render();
+    console.log('index route achieved');
+  }
 });
 
 var BeerView = Backbone.View.extend({
+  $el: $(this.el),
+
   events: {
-            "click button" : "eventTest"
-          },
+    "click #button2" : "eventTest"
+  },
+
   eventTest: function() {
-                alert('Ride like the wind!');
-              },
+    alert('Ride like the wind!');
+  },
+
+  render: function() {
+    this.$el.append("<button type='button' id='button2'>button!</button><p class='beerclass'>I'm a beer view!</p>");
+    return this;
+  },
+
   initialize: function() { 
-              $('#main').append("<button type='button'>button!</button><p class='beerclass'>I'm a beer view!</p>");
-              }
+    _.bindAll(this, 'render');
+  }
 
 });
 //model
@@ -32,6 +43,5 @@ $(document).ready(function() {
   //start it up here!
   var router = new BeerRouter();
   Backbone.history.start();
-  var bv = new BeerView();
   //bv.initialize();
 });
